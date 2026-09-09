3d62d33946f67ed3a12cd9b19bab0be0e43160f3
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-09-09 19:35:38 +0530

    type

---
+++ Python/Basics/typeconversion.py
@@ -0,0 +1,21 @@
+# age  = input("Enter Your Age: ")
+
+
+# # print(age, type(age));
+
+# # print(age +1)
+
+# # int(), float()
+
+# new_age = int(age) +1
+# print(new_age)
+# print(float(new_age))
+
+
+# Sum Program
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+sum =  a + b
+print("sum:", sum)
