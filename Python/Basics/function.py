ff62287146c53459a780482775ebd3955417843e
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-09-15 08:08:58 +0530

    Add initial implementation of functions for GST calculation and summation in function.py

---
+++ Python/Basics/function.py
@@ -0,0 +1,16 @@
+# price = 100
+
+# new_price = price + (price *0.18)
+# print(new_price)
+
+# def sum(a,b):
+#     print(a+b)
+
+# sum(1,2)
+
+# def calc_gst(price):
+#     new_price = price + price *0.18
+#     print(new_price)
+
+# calc_gst(100)
+# calc_gst(2050)
