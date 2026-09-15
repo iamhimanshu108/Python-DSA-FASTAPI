01d3a28d9ebed30c8bf00cd2da4ccbfcaa08c097
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-09-15 07:21:50 +0530

    Add initial implementation of list operations and examples in list.py

---
+++ Python/Basics/list.py
@@ -0,0 +1,22 @@
+# marks= [98,99,97,95,93,"A", 2.5]
+
+# print(marks, type(marks))
+
+# #length
+# print(len(marks))
+
+# print(marks[0])
+# print(marks[-1])
+
+# #slice
+# print(marks[0:3])
+# print(marks[-3:])
+# print(marks[:2])
+
+# marks= [98,99,97,95,93]
+
+# for score in marks:
+#     print(score)
+
+# marks.append(50)
+# print(marks)
